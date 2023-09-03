b4674d6db1b290ca4e482fbf650d58a41f150715
Erhan Gündüz <erhangndz@live.com>
2023-09-03 12:03:04 +0300

    Add project files.

---
+++ WebApıJWT/Controllers/DefaultController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WebApıJWT.Models;
+
+namespace WebApıJWT.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DefaultController : ControllerBase
+    {
+
+        [HttpGet("[action]")]
+        public IActionResult CreateToken()
+        {
+            return Ok(new CreateToken().TokenCreate());
+        }
+
+        [HttpGet("[action]")]
+        public IActionResult CreateTokenforAdmin()
+        {
+            return Ok(new CreateToken().CreateTokenAdmin());
+        }
+
+        [Authorize]
+        [HttpGet("[action]")]
+        public IActionResult Test2()
+        {
+            return Ok("Hoş Geldiniz");
+        }
+
+        [Authorize(Roles ="Admin,Visitor")]
+        [HttpGet("[action]")]
+        public IActionResult Test3()
+        {
+            return Ok("Token başarılı bir şekilde giriş yaptı");
+        }
+    }
+}
